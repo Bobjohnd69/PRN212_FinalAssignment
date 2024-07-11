a00e80167aa1939e0533ffa9cf0fc8ec943dd8cc
Van Quoc Thinh <thinhvan.231003@gmail.com>
2024-07-12 00:21:12 +0700

    done x2

--- PROJECT/obj/Debug/net8.0-windows/PROJECT.AssemblyInfo.cs
+++ PROJECT/obj/Debug/net8.0-windows/PROJECT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PROJECT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da741374f770866af7e86f3620e5199374cb6eaf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d80a79139a3ea5a8632237e76e17aa6119bd54d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PROJECT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PROJECT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
